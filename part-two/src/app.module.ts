b03617d234aa2c4238c312f37db8398f3db01a7d
Donadams50 <sumbomatic@gmail.com>
2022-02-17 22:38:21 +0100

    completed part-two

--- part-two/src/app.module.ts
+++ part-two/src/app.module.ts
@@ -6,13 +6,13 @@ import { AppService } from './app.service';
 import { AuthorModule } from './author/author.module';
 import { BookModule } from './book/book.module';
 import { ConfigModule } from '@nestjs/config';
-ConfigModule.forRoot()
+ConfigModule.forRoot();
 
-process.env.DB_URL
+process.env.DB_URL;
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb+srv://donadams50:mathematics@ahiajara.szq4a.mongodb.net/nestjs-graphql?retryWrites=true&w=majority'),
+    MongooseModule.forRoot(process.env.DB_URL),
     GraphQLModule.forRoot({
       autoSchemaFile: 'schema.gql',
     }),
